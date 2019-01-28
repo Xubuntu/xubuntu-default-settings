e74c3ef24752b4bb1096bd3cd4b15c0f23d72280
Sean Davis <smd.seandavis@gmail.com>
2019-01-28 06:21:19 -0500

    Add Terminal (F4) and Find (Primary+Shift+F) shortcuts to Thunar (LP: #1793395)

---
+++ etc/xdg/xdg-xubuntu/Thunar/accels.scm
@@ -0,0 +1,4 @@
+(gtk_accel_path "<Actions>/ThunarActions/uca-action-1-1" "F4")
+(gtk_accel_path "<Actions>/ThunarActions/uca-action-2-2" "F4")
+(gtk_accel_path "<Actions>/ThunarActions/uca-action-3-3" "<Primary><Shift>f")
+
