dab9902e2cb15a90e13c7066048b55e6ae0e9c78
Sean Davis <sean@bluesabre.org>
2022-09-15 00:36:31 -0400

    Restore origin schedule and branch

--- .github/lpbugtracker.py
+++ .github/lpbugtracker.py
@@ -16,7 +16,7 @@ from launchpadlib.launchpad import Launchpad
 log = logging.getLogger("lpbugtracker")
 log.setLevel(logging.DEBUG)
 
-GH_OWNER = "bluesabre"
+GH_OWNER = "Xubuntu"
 GH_REPO = "xubuntu-default-settings"
 
 LP_SOURCE_NAME = "xubuntu-default-settings"
