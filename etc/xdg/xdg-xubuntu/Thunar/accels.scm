cf9754cbe2def1fbeea5a6678b803906429f2039
Sean Davis <sean@bluesabre.org>
2022-09-24 07:58:34 -0400

    Thunar/uca.xml.in: Maintain previous ids to prevent keyboard accelerator shifts

--- etc/xdg/xdg-xubuntu/Thunar/accels.scm
+++ etc/xdg/xdg-xubuntu/Thunar/accels.scm
@@ -1,4 +1,3 @@
 (gtk_accel_path "<Actions>/ThunarActions/uca-action-1-1" "F4")
 (gtk_accel_path "<Actions>/ThunarActions/uca-action-2-2" "F4")
 (gtk_accel_path "<Actions>/ThunarActions/uca-action-3-3" "<Primary><Shift>f")
-
